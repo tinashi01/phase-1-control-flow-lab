81ca7aa02ad84dc72a2b86e38ac7ff1a4e7a7889
Tina Shi <init.defaultBranch>
2023-03-07 17:47:19 +1100

    commit tree a60b6444aeeffb0920b978a667757a8780db7e19

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 199) {
+    return 'This one is on me!'
+  }
+  else if (someValue <= 1500) {
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue >= 2501) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.':'No go.'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
